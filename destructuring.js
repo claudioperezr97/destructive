ea3b7ac1f7728febaf87c25d6adbfd35d3475558
claudioperezr97 <117235760+claudioperezr97@users.noreply.github.com>
2022-11-10 16:30:14 -0600

    Add files via upload
    Add files

---
+++ destructuring.js
@@ -0,0 +1,84 @@
+//Exercise 1
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); //8
+console.log(yearNeptuneDiscovered); // 1846
+
+//Exercise Number 2
+
+let planetFacts = {
+    numPlanets: 8,
+    yearNeptuneDiscovered: 1846,
+    yearMarsDiscovered: 1659
+  };
+  
+let {numPlanets_2, ...discoveryYears} = planetFacts;
+  
+console.log(discoveryYears); // {yearNeptuneDiscovered:1846, yearMarsDiscovered:1659}
+
+// Exercise 3
+
+function getUserData({firstName, favoriteColor="green"}){
+    return `Your name is ${firstName} and you like ${favoriteColor}`;
+  }
+  
+  getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // `Your name is Alejandro and you like purple`
+  getUserData({firstName: "Melissa"}) // `Your name is Melissa and you like green`
+  getUserData({}) // `Your name is undefined and you like green`
+
+
+//Exercise 4
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // "Maya"
+console.log(second); // "Marisa"
+console.log(third); // "Chi"
+
+//Exercise 5
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+  ]
+  
+  console.log(raindrops); // "Raindrops on roses"
+  console.log(whiskers); // "Whiskers on kittens"
+  console.log(aFewOfMyFavoriteThings); // ["Bright Copper kettles", "Warm Woolen Mittens", "Brownn paper packages"]
+
+//Exercise 6
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+
+console.log(numbers) // [10,30,20]
+
+//Exercise 7
+/*
+var obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+  var a = obj.numbers.a;
+  var b = obj.numbers.b;
+*/
+
+const {numbers:{a, b}} = obj
+
+//Exercise 8
+/*
+var arr = [1, 2];
+var temp = arr[0];
+arr[0] = arr[1];
+arr[1] = temp;
+*/
+
+const arr = [1, 2]
+[arr[0], arr[1]] = [arr[1], arr[0]]
+
+
+
